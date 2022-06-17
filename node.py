b13dfecf832376112069523f410b4a03c748674d
siwalan <59278902+siwalan@users.noreply.github.com>
2022-06-18 00:54:49 +0800

    Update node.py

--- node.py
+++ node.py
@@ -1,18 +1,13 @@
 import numpy as np
 
 class Node(object):
-    def __init__(self, params={'x':0.0, 'y':0.0}):
-        self.params = params
-        if 'x' not in self.params:
-            self.params['x']  = 0.0
-        if 'y' not in self.params:
-            self.params['y'] = 0.0
-
+    def __init__(self, x,y):
         self.id = -1
-        self.pos = np.array([params['x'],params['y']])
+        self.pos = np.array([x,y])
         self.disp = np.array([0.0, 0.0])
         self.force = np.array([0.0, 0.0])
         self.fixity = [False,False]
+        self._hasLoad = False
 
     def __str__(self):
         s = "Node({})".format(self.id)
@@ -23,10 +18,10 @@ class Node(object):
 
     def getNode(self):
         return self.id
-    def fixdof(self,dof):
+    def fixDOF(self,dof):
         self.fixity[dof] = True
 
-    def isfixed(self,dof):
+    def isFixed(self,dof):
         return self.fixity[dof]
 
     def setDisp(self,u,v):
@@ -43,20 +38,14 @@ class Node(object):
 
     def addLoad(self,Px,Py):
         self.force += np.array([Px,Py])
+        self._hasLoad = True
 
     def setLoad(self,Px,Py):
         self.force = np.array([Px,Py])
+        self._hasLoad = True
 
     def getLoad(self):
         return self.force
 
-if __name__ == "__main__":
-    # testing the Node class
-    Nodal = Node(params={'x':25.0, 'y':10})
-
-    Nodal.pos
-    Nodal.disp
-    Nodal.force
-
-    Nodal.addLoad(200,300)
-    print(Nodal.getLoad())
+    def hasLoad(self):
+        return self._hasLoad
